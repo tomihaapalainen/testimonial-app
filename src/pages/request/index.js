bdc57595a2ec10e3c10d3f5f1a0dc89eaf9d435d
Tomi Haapalainen <tomi.haapalainen@protonmail.com>
2021-10-19 20:50:19 +0300

    update

--- src/pages/request/index.js
+++ src/pages/request/index.js
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
-import { useHistory, useParams } from "react-router";
+import { useParams } from "react-router";
 
 const RequestPage = () => {
   const { id } = useParams();
-
-  useEffect(() => {
-    console.log(id);
-  }, [id]);
 
   return <div>ID: {id}</div>;
 };
