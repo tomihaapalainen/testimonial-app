d25f11abf8c8cf321e421e586ff1216155cd338a
Tomi Haapalainen <tomi.haapalainen@protonmail.com>
2021-10-24 13:56:36 +0300

    Add testimonial giver information form to video testimonial preview.

--- src/pages/request/components/VideoTestimonial.js
+++ src/pages/request/components/VideoTestimonial.js
@@ -3,13 +3,35 @@ import { FaStop, FaVideo } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import Webcam from "react-webcam";
 import ReactPlayer from "react-player";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { baseUrl } from "../../../config";
+import { useHistory } from "react-router";
 
 const VideoTestimonial = ({ request, cancel }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const history = useHistory();
+
   const webcamRef = useRef(null);
   const mediaRecorderRef = useRef(null);
   const [capturing, setCapturing] = useState(false);
   const [recordedChunks, setRecordedChunks] = useState([]);
   const [objectURL, setObjectURL] = useState(null);
+  const [videoBlob, setVideoBlob] = useState(null);
+
+  const handleDataAvailable = useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
 
   const handleStartCaptureClick = useCallback(async () => {
     setCapturing(true);
@@ -21,21 +43,12 @@ const VideoTestimonial = ({ request, cancel }) => {
       handleDataAvailable
     );
     mediaRecorderRef.current.start();
-  }, [webcamRef, setCapturing, mediaRecorderRef]);
-
-  const handleDataAvailable = useCallback(
-    ({ data }) => {
-      if (data.size > 0) {
-        setRecordedChunks((prev) => prev.concat(data));
-      }
-    },
-    [setRecordedChunks]
-  );
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
 
   const handleStopCaptureClick = useCallback(() => {
     mediaRecorderRef.current.stop();
     setCapturing(false);
-  }, [mediaRecorderRef, webcamRef, setCapturing]);
+  }, [mediaRecorderRef, setCapturing]);
 
   const handlePreview = useCallback(() => {
     if (recordedChunks.length) {
@@ -49,29 +62,114 @@ const VideoTestimonial = ({ request, cancel }) => {
 
   const handleRecordAgain = () => {
     setObjectURL(null);
+    setVideoBlob(null);
     setRecordedChunks([]);
+  };
+
+  const onSubmit = async (data) => {
+    const { username, usertitle, userbusiness } = data;
+
+    try {
+      const data = new FormData();
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      data.append("image", blob, "video.webm");
+      const uploadResponse = await axios.post(
+        `${baseUrl}/testimonial/video`,
+        data,
+        {
+          headers: { "Content-Type": "multipart/form-data;" },
+        }
+      );
+      if (uploadResponse.status === 200) {
+        const saveResponse = await axios.post(`${baseUrl}/testimonial/new`, {
+          giver_name: username,
+          giver_title: usertitle,
+          business_name: userbusiness,
+          business_identity: request.business_identity,
+          video_url: uploadResponse.data.url,
+        });
+
+        if (saveResponse.status === 201) {
+          history.push("/thankyou");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   if (objectURL) {
     return (
       <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col w-10/12 justify-center items-center max-w-screen-sm">
-          <ReactPlayer url={objectURL} controls />
-        </div>
-        <div className="w-10/12 max-w-sm flex flex-col items-center mt-5 space-y-5 sm:space-y-10">
-          <button
-            onClick={handleRecordAgain}
-            className="w-full flex justify-center border border-gray-400 px-5 py-3 text-gray-800 hover:bg-white hover:shadow-md"
-          >
-            Kuvaa uudestaan
-          </button>
-          <button
-            onClick={handleStartCaptureClick}
-            className="w-full flex justify-center px-5 py-3 font-bold text-white bg-green-600 hover:bg-green-500 hover:shadow-md"
-          >
-            Lähetä
-          </button>
-        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-10/12 max-w-screen-md space-y-3 flex flex-col justify-center items-center"
+        >
+          <div className="w-full flex flex-col">
+            <div>
+              <label className="text-sm text-gray-700">Nimesi*</label>
+              <div className="w-full border-b border-gray-300 rounded-sm focus-within:border-gray-600">
+                <input
+                  {...register("username", { required: true })}
+                  type="text"
+                  onFocus={(e) => e.target.select()}
+                  className="w-full text-gray-800 bg-gray-50 appearance-none outline-none bg-none p-1"
+                />
+              </div>
+              <div className="h-6">
+                {errors.username && (
+                  <p className="text-red-600 text-xs">Täytä nimesi</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label className="text-sm text-gray-700">Tittelisi*</label>
+              <div className="w-full border-b border-gray-300 rounded-sm focus-within:border-gray-600">
+                <input
+                  {...register("usertitle", { required: true })}
+                  type="text"
+                  onFocus={(e) => e.target.select()}
+                  className="w-full text-gray-800 bg-gray-50 appearance-none outline-none bg-none p-1"
+                />
+              </div>
+              <div className="h-6">
+                {errors.usertitle && (
+                  <p className="text-red-600 text-xs">Täytä tittelisi</p>
+                )}
+              </div>
+            </div>
+            <div>
+              <label className="text-sm text-gray-700">Yrityksesi</label>
+              <div className="w-full border-b border-gray-300 rounded-sm focus-within:border-gray-600">
+                <input
+                  {...register("userbusiness")}
+                  type="text"
+                  onFocus={(e) => e.target.select()}
+                  className="w-full text-gray-800 bg-gray-50 appearance-none outline-none bg-none p-1"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-10/12 justify-center items-center max-w-screen-sm">
+            <ReactPlayer url={objectURL} controls />
+          </div>
+          <div className="w-10/12 max-w-sm flex flex-col items-center mt-5 sm:mt-10 space-y-5 sm:space-y-10">
+            <button
+              onClick={handleRecordAgain}
+              className="w-full flex justify-center border border-gray-400 px-5 py-3 text-gray-800 hover:bg-white hover:shadow-md"
+            >
+              Kuvaa uudestaan
+            </button>
+            <button
+              type="submit"
+              className="w-full flex justify-center px-5 py-3 font-bold text-white bg-green-600 hover:bg-green-500 hover:shadow-md"
+            >
+              Lähetä
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
@@ -108,14 +206,9 @@ const VideoTestimonial = ({ request, cancel }) => {
           {recordedChunks.length > 0 && (
             <button
               onClick={handlePreview}
-              className="w-full flex justify-center border border-gray-400 px-5 py-3 text-gray-800 hover:shadow-md hover:bg-white"
+              className="w-full flex justify-center px-5 py-3 font-bold text-white bg-green-600 hover:bg-green-500 hover:shadow-md"
             >
-              <p>Esikatsele</p>
-            </button>
-          )}
-          {recordedChunks.length > 0 && (
-            <button className="w-full flex justify-center px-5 py-3 font-bold text-white bg-green-600 hover:bg-green-500 hover:shadow-md">
-              <p>Lähetä</p>
+              <p>Jatka</p>
             </button>
           )}
         </div>
